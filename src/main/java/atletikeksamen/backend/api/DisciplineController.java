553a4d9bddbfdf78a66affc38f2c05e4c9d63bde
Jasa95 <jaco3020@stud.kea.dk>
2024-06-20 21:49:44 +0200

    Update DisciplineController.java

--- src/main/java/atletikeksamen/backend/api/DisciplineController.java
+++ src/main/java/atletikeksamen/backend/api/DisciplineController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/discipline")
+@RequestMapping("/api/disciplines")
 public class DisciplineController {
     @Autowired
     private DisciplineService disciplineService;
